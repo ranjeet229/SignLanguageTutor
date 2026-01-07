48e0d01489a0fee848dcf67b44e437ddbeb6f0b0
Ranjeet <kiranjeetkr80@gmail.com>
2026-01-07 19:54:08 +0530

    setup the react components

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Sign Language Tutor</h1>
+      <p>Learn sign language using AI </p>
+    </div>
+  );
+};
+
+export default Home;
